bfba19f51e8de3eb40b49b2485f8385c2117d21b
Angel Breaux <abreaux@detaso.com>
2020-12-15 11:36:42 -0600

    Add more network tests

--- test/network_test.rb
+++ test/network_test.rb
@@ -132,14 +132,16 @@ class NetworkTest < Minitest::Test
 
   def test_prolific_actors
     nbc = Network.new("NBC")
+
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
     mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
     baywatch = Show.new("Baywatch", "Gregory Bonann", [mitch])
+
     leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
-    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
     parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
 
     nbc.add_show(knight_rider)
@@ -147,6 +149,12 @@ class NetworkTest < Minitest::Test
     nbc.add_show(parks_and_rec)
 
     assert_equal ["David Hasselhoff"], nbc.prolific_actors
+
+    mrs_george = Character.new({name: "Mrs. George", actor: "Amy Poehler", salary: 2_000_000})
+    mean_girls = Show.new("Mean Girls", "Mark Waters", [mrs_george])
+    nbc.add_show(mean_girls)
+
+    assert_equal ["David Hasselhoff", "Amy Poehler"], nbc.prolific_actors
   end
 
 end
